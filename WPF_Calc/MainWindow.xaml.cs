f7ace46a8ee8fb4dc1c65d46c3ca8b4bfe877569
SimonAi1801 <56634821+SimonAi1801@users.noreply.github.com>
2020-09-22 21:23:38 +0200

    implemented code behind

--- WPF_Calc/MainWindow.xaml.cs
+++ WPF_Calc/MainWindow.xaml.cs
@@ -20,9 +20,248 @@ namespace WPF_Calc
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double _result;
+        private string _first;
+        private string _second;
+        private string _operator;
         public MainWindow()
         {
             InitializeComponent();
+            btnNum0.Click += Btn_Click;
+            btnNum1.Click += Btn_Click;
+            btnNum2.Click += Btn_Click;
+            btnNum3.Click += Btn_Click;
+            btnNum4.Click += Btn_Click;
+            btnNum5.Click += Btn_Click;
+            btnNum6.Click += Btn_Click;
+            btnNum7.Click += Btn_Click;
+            btnNum8.Click += Btn_Click;
+            btnNum9.Click += Btn_Click;
+            btnEqual.Click += Btn_Click;
+            btnC.Click += Btn_Click;
+            btnDiv.Click += Btn_Click;
+            btnMinus.Click += Btn_Click;
+            btnMult.Click += Btn_Click;
+            btnPlus.Click += Btn_Click;
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+
+            switch (button.Content.ToString())
+            {
+                case "C":
+                    {
+                        _first = null;
+                        _operator = null;
+                        _second = null;
+                        _result = 0;
+                        txtResult.Text = _result.ToString();
+                        break;
+                    }
+                case "+":
+                    {
+                        _operator = "+";
+                        txtResult.Text = _operator;
+                        break;
+                    }
+
+                case "-":
+                    {
+                        _operator = "-";
+                        txtResult.Text = _operator;
+                        break;
+                    }
+
+
+                case "*":
+                    {
+                        _operator = "*";
+                        txtResult.Text = _operator;
+                        break;
+                    }
+
+                case "/":
+                    {
+                        _operator = "/";
+                        txtResult.Text = _operator;
+                        break;
+                    }
+                case "=":
+                    {
+                        if (_operator == null)
+                        {
+                            txtResult.Text = "0";
+                            _first = null;
+                            _second = null;
+                            _result = 0;
+                        }
+                        else
+                        {
+                            if (_operator == "+")
+                            {
+                                _result = Convert.ToInt32(_first) + Convert.ToInt32(_second);
+                            }
+                            else if (_operator == "-")
+                            {
+                                
+                            }
+                            else if (_operator == "*")
+                            {
+
+                            }
+                            else if (_operator == "/")
+                            {
+                                if (_second == "0")
+                                {
+                                
+                                }
+                            }
+                        }
+                        break;
+                    }
+                case "1":
+                    {
+                        if (_operator == null)
+                        {
+                            _first += "1";
+                            txtResult.Text = _first.ToString();
+                        }
+                        else
+                        {
+                            _second += "1";
+                            txtResult.Text = _second.ToString();
+                        }
+                        break;
+                    }
+                case "2":
+                    {
+                        if (_operator == null)
+                        {
+                            _first += "2";
+                            txtResult.Text = _first.ToString();
+                        }
+                        else
+                        {
+                            _second += "2";
+                            txtResult.Text = _second.ToString();
+                        }
+                        break;
+                    }
+                case "3":
+                    {
+                        if (_operator == null)
+                        {
+                            _first += "3";
+                            txtResult.Text = _first.ToString();
+                        }
+                        else
+                        {
+                            _second += "3";
+                            txtResult.Text = _second.ToString();
+                        }
+                        break;
+                    }
+                case "4":
+                    {
+                        if (_operator == null)
+                        {
+                            _first += "4";
+                            txtResult.Text = _first.ToString();
+                        }
+                        else
+                        {
+                            _second += "4";
+                            txtResult.Text = _second.ToString();
+                        }
+                        break;
+                    }
+                case "5":
+                    {
+                        if (_operator == null)
+                        {
+                            _first += "5";
+                            txtResult.Text = _first.ToString();
+                        }
+                        else
+                        {
+                            _second += "5";
+                            txtResult.Text = _second.ToString();
+                        }
+                        break;
+                    }
+                case "6":
+                    {
+                        if (_operator == null)
+                        {
+                            _first += "6";
+                            txtResult.Text = _first.ToString();
+                        }
+                        else
+                        {
+                            _second += "6";
+                            txtResult.Text = _second.ToString();
+                        }
+                        break;
+                    }
+                case "7":
+                    {
+                        if (_operator == null)
+                        {
+                            _first += "7";
+                            txtResult.Text = _first.ToString();
+                        }
+                        else
+                        {
+                            _second += "7";
+                            txtResult.Text = _second.ToString();
+                        }
+                        break;
+                    }
+                case "8":
+                    {
+                        if (_operator == null)
+                        {
+                            _first += "8";
+                            txtResult.Text = _first.ToString();
+                        }
+                        else
+                        {
+                            _second += "8";
+                            txtResult.Text = _second.ToString();
+                        }
+                        break;
+                    }
+                case "9":
+                    {
+                        if (_operator == null)
+                        {
+                            _first += "9";
+                            txtResult.Text = _first.ToString();
+                        }
+                        else
+                        {
+                            _second += "9";
+                            txtResult.Text = _second.ToString();
+                        }
+                        break;
+                    }
+                case "0":
+                    {
+                        if (_operator == null)
+                        {
+                            _first += "0";
+                            txtResult.Text = _first.ToString();
+                        }
+                        else
+                        {
+                            _second += "0";
+                            txtResult.Text = _second.ToString();
+                        }
+                        break;
+                    }
+            }
         }
     }
 }
